23cdd2d470e88893c120ca144c4cb6f93df21472
Jérôme Combes <jerome@planningbiblio.fr>
2015-03-18 15:21:06 +0100

    Corrige le fichier lang/fr.js

--- lang/fr.js
+++ lang/fr.js
@@ -8,8 +8,8 @@ CJLang.intro1="Avant de remplir le formulaire approprié, vous devez avoir déj�
 <a href='mailto:parisadministration.cgc@columbia.edu'>parisadministration.cgc@columbia.edu</a><br/><br/>\
 Veuillez choisir votre formulaire :\
 <ul>\
-<li><a href='http://www.reidhall.com/informations/membres' target='_blank'>Si vous êtes un membre de reidhall</a></li>\
-<li><a href='http://www.reidhall.com/informations' target='_blank'>Si vous n'êtes pas membre de reidhall</a></li>\
+<li><a href='http://www.reidhall.com/informations/membres' target='_blank'>Si vous êtes un membre de Reid Hall</a></li>\
+<li><a href='http://www.reidhall.com/informations' target='_blank'>Si vous n'êtes pas membre de Reid Hall</a></li>\
 <li><a href='http://www.reidhall.com/information/2015-01' target='_blank'>Si vous êtes affilié à Columbia University</a></li>\
 </ul>";
 
