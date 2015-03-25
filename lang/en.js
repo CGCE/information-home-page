4501861deec594b0cd36e3a634deb907dd4b7379
Jérôme Combes <jerome@planningbiblio.fr>
2015-03-25 15:58:53 +0100

    Corrige le lien if you are a from -> If you are from

--- lang/en.js
+++ lang/en.js
@@ -10,7 +10,7 @@ Please, choose your information form :\
 <ul>\
 <li><a href='http://www.reidhall.com/informations/membres' target='_blank'>If you are a member of Reid Hall, click here</a></li>\
 <li><a href='http://www.reidhall.com/informations' target='_blank'>If you are not a member of Reid Hall, click here</a></li>\
-<li><a href='http://www.reidhall.com/information/2015-02' target='_blank'>If you are a from Columbia University New York, click here</a></li>\
+<li><a href='http://www.reidhall.com/information/2015-02' target='_blank'>If you are from Columbia University New York, click here</a></li>\
 <li><a href='http://www.reidhall.com/information/2015-01' target='_blank'>If you are a Columbia University affiliate, click here</a></li>\
 </ul>";
 
